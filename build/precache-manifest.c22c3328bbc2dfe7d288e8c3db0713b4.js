d92e278efa1bcdac7ca2b3dadadcc829e721c14b
unknown <70shakeel@gmail.com>
2020-05-04 13:44:06 +0500

    build version

---
+++ build/precache-manifest.c22c3328bbc2dfe7d288e8c3db0713b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4744f37d5e5720f94ddc572da6b0dc14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ccba101b129add398a7",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ee912f32c57fcce355c7",
+    "url": "/static/js/2.10775801.chunk.js"
+  },
+  {
+    "revision": "7ccba101b129add398a7",
+    "url": "/static/js/main.0bf1347a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
